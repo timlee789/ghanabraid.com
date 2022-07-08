3b0f6962c0d00249860b7272bf48e20a9342825d
timlee789 <systeco@hotmail.com>
2022-07-08 11:48:04 -0400

    0708-3

--- components/caribbeanstyle.js
+++ components/caribbeanstyle.js
@@ -1,9 +1,6 @@
-import cla from '../styles/form.module.css';
-import {useRef, useState} from 'react';
-import {useRouter} from 'next/router'
-import axios from 'axios';
-import StoreList from './storeList';
-import Head from 'next/head';
+import cla from './style.module.css';
+import Link from 'next/link'
+
 
 export default function Caribbeanstyle({storename}) {
   
@@ -14,40 +11,315 @@ export default function Caribbeanstyle({storename}) {
       <main className={cla.body}>
      
         
-       <div className={cla.title}>
-        3X Ghana Braid Giveaway Event
-       </div>
-      
-       <div className={cla.phototitle}>Realistic 3X Ghana Braid</div>
-       <div className={cla.album}>
-       
-       <img src='https://bijouxhair.com/tim/landing2/3xghanaphoto6.jpg' alt='banner' className={cla.image}/>
-       <div className={cla.videotitle}>3X Ghana Braid is Highend Braid</div>
-       <img src='https://bijouxhair.com/tim/landing2/3xghanaphoto5.jpg' alt='banner' className={cla.image}/>
-       <div className={cla.videotitle}>3X Ghana Braid 100% Kanekalon </div>
-      
-       <div className={cla.videotitle}>3X Ghana Braid in Bahamas Cable Beach</div>
-       <img src='https://bijouxhair.com/tim/landing2/3xghanacolor.jpg' alt='banner' className={cla.image}/>
-       <div className={cla.videotitle}>3X Ghana Braid over 50 different colors</div>
-       <img src='https://bijouxhair.com/tim/landing2/3xghanapackage.jpg' alt='banner' className={cla.image}/>
-       <div className={cla.videotitle}>3X Ghana Braid 40 50 60 70</div>
-       
-       <div className={cla.videotitle}>3X Ghana Braid in Kingston Jamaica</div>
-       <img src='https://bijouxhair.com/tim/landing2/3xghanaphoto2.jpg' alt='banner' className={cla.image}/>
-       <div className={cla.videotitle}> Try new style with 3X Ghana Braid  </div>
-       
-       <div className={cla.videotitle}>3X Ghana Braid in Wynwood Miami FL</div>
-       <img src='https://bijouxhair.com/tim/landing2/3xghanaphoto1.jpg' alt='banner' className={cla.image}/>
-      
-       <img src='https://bijouxhair.com/tim/landing2/3xghanaspec.jpg' alt='banner' className={cla.image}/>
-       
-       <div className={cla.footer}>
-        <div className={cla.footimage}>
-        <a href="https://bijouxhair.com/" target='_blank' rel="noreferrer"
-          ><img alt=""  src="https://bijouxhair.com/catalog/webmain/beautyelementlogo.jpg" /></a>
+      <div className={cla.winners}>
+    <div className={cla.card}>
+      <div className={cla.title2}>Braid Winner</div>
+          <div className={cla.imgback2}>
+          <Link href="https://bijouxhair.com/tim/pc/Christelle2.jpg" target='_blank' >
+            <img src='https://bijouxhair.com/tim/pc/Christelle2.jpg' alt="tt"   className={cla.imgobject2} />
+          </Link>
+          </div>
+        <div className={cla.headbox2}>
+        <div className={cla.headletter4}>First Place</div>
+        <div className={cla.headletter}>
+          Christelle MONFISTON
+        </div>
+        <div className={cla.headletter3}>Haiti</div>
       </div>
       </div>
-       </div>
+      <div className={cla.card}>
+        <div className={cla.title2}>Braid Winner</div>
+            <div className={cla.imgback2}>
+            <Link href="https://bijouxhair.com/tim/pc/Christelle3.jpg" target='_blank' >
+              <img src='https://bijouxhair.com/tim/pc/Christelle3.jpg' alt="tt"   className={cla.imgobject2} />
+            </Link>
+            </div>
+          <div className={cla.headbox2}>
+          <div className={cla.headletter4}>First Place</div>
+          <div className={cla.headletter}>
+            Christelle MONFISTON
+          </div>
+          <div className={cla.headletter3}>Haiti</div>
+        </div>
+        </div>
+        <div className={cla.card}>
+          <div className={cla.title2}>Braid Winner</div>
+              <div className={cla.imgback2}>
+              <Link href="https://bijouxhair.com/tim/pc/Georgett.jpg" target='_blank' >
+                <img src='https://bijouxhair.com/tim/pc/Georgett.jpg' alt="tt"   className={cla.imgobject2} />
+              </Link>
+              </div>
+            <div className={cla.headbox2}>
+            <div className={cla.headletter4}>Second Place</div>
+            <div className={cla.headletter}>
+              Georgett Watson
+            </div>
+            <div className={cla.headletter3}>Jamaica</div>
+          </div>
+          </div>
+          <div className={cla.card}>
+            <div className={cla.title2}>Braid Winner</div>
+                <div className={cla.imageback2}>
+                <Link href="https://bijouxhair.com/tim/pc/Georgett3.jpg" target='_blank' >
+                  <img src='https://bijouxhair.com/tim/pc/Georgett3.jpg' alt="tt"   className={cla.imgobject2} />
+                </Link>
+                </div>
+              <div className={cla.headbox2}>
+              <div className={cla.headletter4}>Second Place</div>
+              <div className={cla.headletter}>
+                Georgett Watson
+              </div>
+              <div className={cla.headletter3}>Jamaica</div>
+            </div>
+            </div>
+            <div className={cla.card}>
+            <div className={cla.title2}>Braid Winner</div>
+            <div className={cla.imageback2}>
+            <Link href="https://bijouxhair.com/tim/pc/Rodline3.jpg" target='_blank' >
+            <img src='https://bijouxhair.com/tim/pc/Rodline3.jpg' alt="tt"   className={cla.imgobject2} />
+            </Link>
+            </div>
+            <div className={cla.headbox2}>
+            <div className={cla.headletter4}>Third Place</div>
+            <div className={cla.headletter}>
+            Rodline St Fleur
+            </div>
+            <div className={cla.headletter3}>Bahamas</div>
+            </div>
+            </div>
+            <div className={cla.card}>
+            <div className={cla.title2}>Braid Winner</div>
+            <div className={cla.imgback2}>
+            <Link href="https://bijouxhair.com/tim/pc/Rodline2.jpg" target='_blank' >
+            <img src='https://bijouxhair.com/tim/pc/Rodline2.jpg' alt="tt"   className={cla.imgobject2} />
+            </Link>
+            </div>
+            <div className={cla.headbox2}>
+            <div className={cla.headletter4}>Third Place</div>
+            <div className={cla.headletter}>
+            Rodline St Fleur
+            </div>
+            <div className={cla.headletter3}>Bahamas</div>
+            </div>
+            </div>
+            <div className={cla.card}>
+            <div className={cla.title2}>Braid Winner</div>
+            <div className={cla.imgback2}>
+            <Link href="https://bijouxhair.com/tim/pc/Stephanie.jpeg" target='_blank' >
+              <img src='https://bijouxhair.com/tim/pc/Stephanie.jpeg' alt="tt"   className={cla.imgobject2} />
+            </Link>
+            </div>
+            <div className={cla.headbox2}>
+            <div className={cla.headletter4}>Fourth Place</div>
+            <div className={cla.headletter}>
+            Stephanie Weir
+            </div>
+            <div className={cla.headletter3}>Trinidad and Tobago</div>
+            </div>
+            </div>
+            <div className={cla.card}>
+            <div className={cla.title2}>Braid Winner</div>
+              <div className={cla.imgback2}>
+              <Link href="https://bijouxhair.com/tim/pc/Stephanie2.jpeg" target='_blank' >
+                <img src='https://bijouxhair.com/tim/pc/Stephanie2.jpeg' alt="tt"   className={cla.imgobject2} />
+              </Link>
+              </div>
+            <div className={cla.headbox2}>
+            <div className={cla.headletter4}>Fourth Place</div>
+            <div className={cla.headletter}>
+              Stephanie Weir
+            </div>
+            <div className={cla.headletter3}>Trinidad and Tobago</div>
+            </div>
+            </div>
+            <div className={cla.card}>
+            <div className={cla.title2}>Braid Winner</div>
+                <div className={cla.imgback2}>
+                <Link href="https://bijouxhair.com/tim/pc/Kadian.jpg" target='_blank' >
+                  <img src='https://bijouxhair.com/tim/pc/Kadian.jpg' alt="tt"   className={cla.imgobject2} />
+                </Link>
+                </div>
+              <div className={cla.headbox2}>
+              <div className={cla.headletter4}>Fifth Place</div>
+              <div className={cla.headletter}>
+                Kadian Mccourty Blackwood
+              </div>
+              <div className={cla.headletter3}>Jamaica</div>
+            </div>
+            </div>
+            <div className={cla.card}>
+              <div className={cla.title2}>Braid Winner</div>
+                  <div className={cla.imgback2}>
+                  <Link href="https://bijouxhair.com/tim/pc/kadian2.jpg" target='_blank' >
+                    <img src='https://bijouxhair.com/tim/pc/kadian2.jpg' alt="tt"   className={cla.imgobject2} />
+                  </Link>
+                  </div>
+                <div className={cla.headbox2}>
+                <div className={cla.headletter4}>Fifth Place</div>
+                <div className={cla.headletter}>
+                  Kadian Mccourty Blackwood
+                </div>
+                <div className={cla.headletter3}>Jamaica</div>
+              </div>
+              </div>
+        </div>
+
+
+         {/* Fantasy winners */}
+    <div className={cla.winners}>          
+      <div className={cla.card}>
+        <div className={cla.title}>Fantasy Winner</div>
+            <div className={cla.imgback2}>
+            <Link href="https://bijouxhair.com/tim/pc/Sade3.jpg" target='_blank' >
+              <img src='https://bijouxhair.com/tim/pc/Sade3.jpg' alt="tt"   className={cla.imgobject2} />
+            </Link>
+            </div>
+          <div className={cla.headbox2}>
+          <div className={cla.headletter4}>First Place</div>
+          <div className={cla.headletter}>
+            Sade Baptiste
+          </div>
+          <div className={cla.headletter3}>Trinidad n Tobago</div>
+        </div>
+        </div>
+        <div className={cla.card}>
+          <div className={cla.title}>Fantasy Winner</div>
+              <div className={cla.imgback2}>
+              <Link href="https://bijouxhair.com/tim/pc/Sade2.jpg" target='_blank' >
+                <img src='https://bijouxhair.com/tim/pc/Sade2.jpg' alt="tt"   className={cla.imgobject2} />
+              </Link>
+              </div>
+            <div className={cla.headbox2}>
+            <div className={cla.headletter4}>First Place</div>
+            <div className={cla.headletter}>
+              Sade Baptiste
+            </div>
+            <div className={cla.headletter3}>Trinidad n Tobago</div>
+          </div>
+          </div>
+      <div className={cla.card}>
+        <div className={cla.title}>Fantasy Winner</div>
+            <div className={cla.imgback2}>
+            <Link href="https://bijouxhair.com/tim/pc/Sade5.jpg" target='_blank' >
+              <img src='https://bijouxhair.com/tim/pc/Sade5.jpg' alt="tt"   className={cla.imgobject2} />
+            </Link>
+            </div>
+          <div className={cla.headbox2}>
+          <div className={cla.headletter4}>First Place</div>
+          <div className={cla.headletter}>
+            Sade Baptiste
+          </div>
+          <div className={cla.headletter3}>Trinidad n Tobago</div>
+        </div>
+        </div>
+        <div className={cla.card}>
+          <div className={cla.title}>Fantasy Winner</div>
+              <div className={cla.imgback2}>
+              <Link href="https://bijouxhair.com/tim/pc/a1.jpeg" target='_blank' >
+                <img src='https://bijouxhair.com/tim/pc/a1.jpeg' alt="tt"   className={cla.imgobject2} />
+              </Link>
+              </div>
+            <div className={cla.headbox2}>
+            <div className={cla.headletter4}>Second Place</div>
+            <div className={cla.headletter}>
+              Alicia Alleyne-lyte
+            </div>
+            <div className={cla.headletter3}>Barbados</div>
+          </div>
+          </div>
+            <div className={cla.card}>
+            <div className={cla.title}>Fantasy Winner</div>
+                <div className={cla.imgback2}>
+                <Link href="https://bijouxhair.com/tim/pc/Nadley.jpg" target='_blank' >
+                  <img src='https://bijouxhair.com/tim/pc/Nadley.jpg' alt="tt"   className={cla.imgobject2} />
+                </Link>
+                </div>
+              <div className={cla.headbox2}>
+              <div className={cla.headletter4}>Third Place</div>
+              <div className={cla.headletter}>
+                Nadley G
+              </div>
+              <div className={cla.headletter3}>Barbados</div>
+            </div>
+            </div>
+    
+            
+            <div className={cla.card}>
+              <div className={cla.title}>Fantasy Winner</div>
+                  <div className={cla.imgback2}>
+                  <Link href="https://bijouxhair.com/tim/pc/Nadley4.jpg" target='_blank' >
+                    <img src='https://bijouxhair.com/tim/pc/Nadley4.jpg' alt="tt"   className={cla.imgobject2} />
+                  </Link>
+                  </div>
+                <div className={cla.headbox2}>
+                <div className={cla.headletter4}>Third Place</div>
+                <div className={cla.headletter}>
+                  Nadley G
+                </div>
+                <div className={cla.headletter3}>Barbados</div>
+              </div>
+              </div>
+              <div className={cla.card}>
+                <div className={cla.title}>Fantasy Winner</div>
+                    <div className={cla.imgback2}>
+                    <Link href="https://bijouxhair.com/tim/pc/Natalie2.jpg" target='_blank' >
+                      <img src='https://bijouxhair.com/tim/pc/Natalie2.jpg' alt="tt"   className={cla.imgobject2} />
+                    </Link>
+                    </div>
+                  <div className={cla.headbox2}>
+                  <div className={cla.headletter4}>Fourth Place</div>
+                  <div className={cla.headletter}>
+                    Natalie Barrow
+                  </div>
+                  <div className={cla.headletter3}>Barbados</div>
+                </div>
+                </div>
+              <div className={cla.card}>
+                <div className={cla.title}>Fantasy Winner</div>
+                    <div className={cla.imgback2}>
+                    <Link href="https://bijouxhair.com/tim/pc/Natalie.jpg" target='_blank' >
+                      <img src='https://bijouxhair.com/tim/pc/Natalie.jpg' alt="tt"   className={cla.imgobject2} />
+                    </Link>
+                    </div>
+                  <div className={cla.headbox2}>
+                  <div className={cla.headletter4}>Fourth Place</div>
+                  <div className={cla.headletter}>
+                    Natalie Barrow
+                  </div>
+                  <div className={cla.headletter3}>Barbados</div>
+                </div>
+                </div>
+              <div className={cla.card}>
+                <div className={cla.title}>Fantasy Winner</div>
+                    <div className={cla.imgback2}>
+                    <Link href="https://bijouxhair.com/tim/pc/Natalie3.jpg" target='_blank' >
+                      <img src='https://bijouxhair.com/tim/pc/Natalie3.jpg' alt="tt"   className={cla.imgobject2} />
+                    </Link>
+                    </div>
+                  <div className={cla.headbox2}>
+                  <div className={cla.headletter4}>Fourth Place</div>
+                  <div className={cla.headletter}>
+                    Natalie Barrow
+                  </div>
+                  <div className={cla.headletter3}>Barbados</div>
+                </div>
+                </div>
+              <div className={cla.card}>
+                <div className={cla.title}>Fantasy Winner</div>
+                    <div className={cla.imgback2}>
+                    <Link href="https://bijouxhair.com/tim/pc/Keanna.jpg" target='_blank' >
+                      <img src='https://bijouxhair.com/tim/pc/Keanna.jpg' alt="tt"   className={cla.imgobject2} />
+                    </Link>
+                    </div>
+                  <div className={cla.headbox2}>
+                  <div className={cla.headletter4}>Fifth Place</div>
+                  <div className={cla.headletter}>
+                    Keanna Mc lean
+                  </div>
+                  <div className={cla.headletter3}>St Lucia</div>
+                </div>
+                </div>
+            </div>
       </main>
 
 </div>
