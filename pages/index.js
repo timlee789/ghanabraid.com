112ad89fc68efb7fb2acc5faa39bafef0833ace4
timlee789 <systeco@hotmail.com>
2022-07-08 13:37:47 -0400

    0708-4

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,17 @@ import cla from '../styles/form.module.css';
 import axios from 'axios';
 import StoreList from '../components/storeList';
 import Head from 'next/head';
+import {MongoClient} from 'mongodb';
+import Loginbtn from '../components/loginbtn';
+import { useState } from 'react';
 
 
 export default function Home({landingdata}) {
-  
+  const [pass, setPass] = useState()
+ 
+  const handleChange =(e) => setPass(e.target.value);
+ 
+
   return (
     <div className={cla.main}>
        <Head>  
@@ -20,9 +27,15 @@ export default function Home({landingdata}) {
     </Head>
       <main className={cla.body}>
       
+       <Loginbtn/>
        <div className={cla.phototitle}>SNS Advertising Stores</div>
        <div className={cla.album}>
-      
+       {/* <form>
+          <input type='text' onChange={handleChange} placeholder='Enter password'/>
+          <button>LOGIN</button>
+        </form> */}
+        
+        
        <StoreList storeList={landingdata} />
        <div className={cla.footer}>
         <div className={cla.footimage}>
@@ -38,17 +51,22 @@ export default function Home({landingdata}) {
 }
 
 export const getServerSideProps = async() => {
-//   const client = await MongoClient.connect(
-//     `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clustertim.koved.mongodb.net/Landing?retryWrites=true&w=majority`
-//     );
-// const db = client.db();
-// const myCollection = db.collection('stores');
-// const users = await myCollection.find({}, {storename: 1}).toArray(); 
-// client.close();
-  const res = await axios.get('https://ghanabraid.com/api/store');
+  const client = await MongoClient.connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clustertim.koved.mongodb.net/Landing?retryWrites=true&w=majority`
+    );
+const db = client.db();
+const myCollection = db.collection('stores');
+const myData = await myCollection.find({}, {storename: 1}).toArray(); 
+client.close();
+  //const res = await axios.get('https://ghanabraid.com/api/store');
   return {
     props: {
-      landingdata: res.data
+      landingdata: myData.map(Data => ({
+        id: Data._id.toString() ,
+        storename: Data.storename || null,
+        img1: Data.img1,
+        desc: Data.desc,
+  }))
       // {
       //   id: users._id,
       //   storename: users.storename,
