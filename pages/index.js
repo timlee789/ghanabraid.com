c2e3bbd3b0f6236ecb1224a4f66278d78d5bc2b4
timlee789 <systeco@hotmail.com>
2022-07-14 13:52:54 -0400

    0714-2

--- pages/index.js
+++ pages/index.js
@@ -14,17 +14,7 @@ export default function Home({landingdata}) {
 
   return (
     <div className={cla.main}>
-       <Head>  
-        {/* <meta name="viewport" content="width=1150"/> */}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>3X Ghana Braid Giveaway Event</title>
-        <meta name="description" content="3X Ghana Braid Giveaway Event" />
-        <link rel="image_src" href="https://bijouxhair.com/tim/landing2/3xghanaphoto6.jpg" />     
-        <meta property="og:title" content="3X Ghana Braid Giveaway Event" />    
-        <meta property="og:image" content="https://bijouxhair.com/tim/landing2/3xghanaphoto6.jpg"/>
-        <meta property="og:description" content="3X Ghana Braid Giveaway Event"/>
-        <meta property="og:site_name" content="Beauty Elements"/> 
-    </Head>
+      
       <main className={cla.body}>
       
        
