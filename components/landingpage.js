80744ee4b9cc22a7c9cb92f7540ac65d1a7b31e2
Tim Lee <systeco@hotmail.com>
2022-10-14 14:52:41 -0400

    1014-3

--- components/landingpage.js
+++ components/landingpage.js
@@ -67,17 +67,23 @@ export default function LandingPage({storename}) {
         
        <div className={cla.ula2}>On Going Free Giveaway Events !</div>
          <div className={cla.imagebtnbox}>
-         <div className={cla.imagebtnbox}>
+        
           <div className={cla.btnimage}  style={{display:"block"}}><button type='button' onClick={handleClick1}>
           <Image src="https://bijouxhair.com/tim/landing2/btnghanabraid2.jpg" alt='ddsl'  width={160} height={120} objectFit='cover'/>
           </button></div>
+          
           <div className={cla.btnimage} ><button type='button' onClick={handleClick5}>
           <Image src="https://bijouxhair.com/tim/landing2/btnghanatwist2.jpg" alt='ddsl'  width={160} height={120} objectFit='cover' />
           </button></div>
-         </div>
+         
          <div className={cla.btnimage} ><button type='button' onClick={handleClick3}>
           <Image src="https://bijouxhair.com/tim/landing2/btndestiny2.jpg" alt='ddsl'  width={160} height={120} objectFit='cover'/>
           </button></div>
+          </div>
+          
+
+  
+          <div className={cla.imagebtnbox}>
           <div className={cla.btnimage} ><button type='button' onClick={handleClick4}>
           <Image src="https://bijouxhair.com/tim/landing2/btntrio2.jpg" alt='ddsl'  width={160} height={120} objectFit='cover'/>
           </button></div>
